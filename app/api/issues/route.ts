df9e7b70e14c2043b5fa17123b91bc4dabdfaea3
Hermawan Hantanto <hermawan.hant@gmail.com>
2023-10-08 16:03:25 +0700

    Handling errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string(),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
 });
 
-// create issue 
+// create issue
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
