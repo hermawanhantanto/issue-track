9b064e28659fbb703c1d4079d31b7d829a093fe7
Hermawan Hantanto <hermawan.hant@gmail.com>
2023-10-10 16:03:33 +0700

    add react-query

--- app/issues/[id]/AssigneSelect.tsx
+++ app/issues/[id]/AssigneSelect.tsx
@@ -1,7 +1,20 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Select, SelectGroup } from "@radix-ui/themes";
+import { User } from "@prisma/client";
+import axios from "axios";
 const AssigneSelect = () => {
+  const [users, setUsers] = React.useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <>
       <Select.Root>
@@ -9,7 +22,13 @@ const AssigneSelect = () => {
         <Select.Content>
           <SelectGroup>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="1">Mosh Hamedani</Select.Item>
+            {users.map((user) => {
+              return (
+                <Select.Item key={user.id} value={user.id}>
+                  {user.name}
+                </Select.Item>
+              );
+            })}
           </SelectGroup>
         </Select.Content>
       </Select.Root>
