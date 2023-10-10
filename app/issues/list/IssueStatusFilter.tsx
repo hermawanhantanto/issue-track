890ece1d1005e107e32fcb9b2605479cf8cd16ab
Hermawan Hantanto <hermawan.hant@gmail.com>
2023-10-10 19:46:36 +0700

    fix filtering bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 import { Status } from "@prisma/client";
 import React from "react";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const status: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -12,13 +12,23 @@ const status: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const params = new URLSearchParams();
+  const searchParams = useSearchParams();
+
   const router = useRouter();
 
   const onStatusChange = (value: string) => {
     if (value === "All") {
-      router.push("/issues/list");
+      if (searchParams.get("orderBy"))
+        params.append("orderBy", searchParams.get("orderBy")!);
+      const query = params.size ? "?" + params.toString() : "";
+      router.push("/issues/list" + query);
     } else {
-      router.push(`/issues/list?status=${value}`);
+      params.append("status", value);
+      if (searchParams.get("orderBy"))
+        params.append("orderBy", searchParams.get("orderBy")!);
+      const query = params.size ? "?" + params.toString() : "";
+      router.push(`/issues/list` + query);
     }
   };
 
@@ -27,11 +37,11 @@ const IssueStatusFilter = () => {
       onValueChange={(value) => {
         onStatusChange(value);
       }}
+      defaultValue={searchParams.get("status") || ""}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         <Select.Group>
-          <Select.Label>Status</Select.Label>
           {status.map((item) => (
             <Select.Item key={item.label} value={item.value || "All"}>
               {item.label}
