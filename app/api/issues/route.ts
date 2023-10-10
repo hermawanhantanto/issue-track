5835ee53ce33a3647380c7402affe2eb42113b0a
Hermawan Hantanto <hermawan.hant@gmail.com>
2023-10-10 10:40:45 +0700

    securing the app

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,9 +1,15 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../../ValidationSchemas";
+import { authOptions } from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 // create issue
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
   const body = await request.json();
 
   const validation = issueSchema.safeParse(body);
